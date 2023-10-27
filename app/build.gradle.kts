a622c474fa92a6c465ec73f5796f8ba9b4b95762
shakajulu82@gmail.com <shakajulu82@gmail.com>
2023-10-27 17:43:33 +0900

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.hussuy.codegod"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hussuy.codegod"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/hussuy/codegod/MainActivity.java
+++ app/src/main/java/com/hussuy/codegod/MainActivity.java
@@ -2,7 +2,12 @@ package com.hussuy.codegod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +22,47 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    public void dialog(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setTitle("알림창");
+        builder.setMessage("전달할 메세지");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "OK 버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("Cancle", null);
+        builder.show();
+    }
+
+    public void custom_dialog(View v) {
+        View dialogView = getLayoutInflater().inflate(R.layout.custom_dl, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+        builder.setView(dialogView);
+
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        Button ok_btn = dialogView.findViewById(R.id.ok_btn);
+        ok_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "OK 버튼을 눌렀습니다.", Toast.LENGTH_LONG).show();
+                alertDialog.dismiss();
+            }
+        });
+
+        Button cancle_btn = dialogView.findViewById(R.id.cancle_btn);
+        cancle_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+    }
+
 }
